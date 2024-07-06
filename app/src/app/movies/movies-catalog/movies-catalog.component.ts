3d6f6c4bc5e5a5e8d8dcea6e095a42d181729ea0
Danilo <danilo.cvijetic10@protonmail.ch>
2024-07-06 21:11:58 +0200

    feat: added search functionality for catalog

--- app/src/app/movies/movies-catalog/movies-catalog.component.ts
+++ app/src/app/movies/movies-catalog/movies-catalog.component.ts
@@ -1,63 +1,76 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { Movie } from '../../models/movie'
-import { AuthService } from "../../services/auth.service";
-import { MovieService } from "../../services/movie.service";
-import { SubscriptionAndFeedInfo } from "../../models/subscription_and_feed_info";
+import { Movie } from '../../models/movie';
+import { AuthService } from '../../services/auth.service';
+import { MovieService } from '../../services/movie.service';
+import { SubscriptionAndFeedInfo } from '../../models/subscription_and_feed_info';
 
 @Component({
   selector: 'app-movies-catalog',
   templateUrl: './movies-catalog.component.html',
-  styleUrls: ['./movies-catalog.component.css']
+  styleUrls: ['./movies-catalog.component.css'],
 })
 export class MoviesCatalogComponent implements OnInit {
-  constructor(private router: Router, private authService: AuthService, private movieService: MovieService) { }
-  movies: Movie[] = []
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private movieService: MovieService,
+  ) { }
+  movies: Movie[] = [];
   searchText: string = '';
-  searchCriteria: string = '';
+  searchCriteria: { name: string, value: string } | null = null;
   criterias = [
     { name: 'Title', value: 'title' },
     { name: 'Genre', value: 'genre' },
     { name: 'Description', value: 'description' },
     { name: 'Actors', value: 'actors' },
-    { name: 'Director', value: 'director' }
+    { name: 'Director', value: 'director' },
   ];
-  role: string = "none";
+  role: string = 'none';
 
   ngOnInit() {
-    this.authService.currentRole.subscribe(role => this.role = role)
+    this.authService.currentRole.subscribe((role) => (this.role = role));
     this.loadMovies();
   }
   addMovie() {
-    this.router.navigate(['/add-movie'])
+    this.router.navigate(['/add-movie']);
   }
   loadMovies() {
-    this.authService.getUserInfo().subscribe({
-      next: (data) => {
-        if (data != null) {
-          // this.movieService.getPersonalizedFeed(data.id).subscribe({
-          //   next: (data) =>{
-          //     console.log(data.Movies)
-          //     this.movies = data.Movies;
-          //   }
-          this.movieService.getAllMovies().subscribe({
-            next: (data) => {
-              console.log(data.Movies)
-              this.movies = data.Movies;
-            }
-          })
-        }
-      },
-      error: err => console.log(err)
-    })
-    // this.movieService.getAllMovies().subscribe({
-    //   next: (data) => {
-    //     console.log(data.Movies)
-    //     this.movies = data.Movies;
-    //
-    //   },
-    //   error: (err) => console.log(err)
-    // })
+    if (this.role == "User") {
+      this.authService.getUserInfo().subscribe({
+        next: (data) => {
+          if (data != null) {
+            this.movieService.getPersonalizedFeed(data.id).subscribe({
+              next: (data) => {
+                console.log(data.Movies)
+                this.movies = data.Movies;
+              }
+            });
+          }
+        },
+        error: (err) => console.log(err),
+      });
+
+    } else if (this.role == "Admin") {
+      this.movieService.getAllMovies().subscribe({
+        next: (data) => {
+          console.log(data.Movies);
+          this.movies = data.Movies;
+        },
+      });
+
+    }
   }
 
+  search() {
+    console.log(this.searchCriteria?.value)
+    console.log(this.searchText)
+    if (this.searchCriteria != null && this.searchText != "") {
+      this.movieService.search(this.searchCriteria['value'], this.searchText).subscribe({
+        next: (results: any) => {
+          this.movies = results.Movies;
+        }
+      });
+    }
+  }
 }
