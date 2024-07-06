2b70b57fbce4244c8611c28a13e6cc354fbb3b9f
Danilo <danilo.cvijetic10@protonmail.ch>
2024-07-06 19:44:53 +0200

    feat: added crew table

--- app/src/app/movies/add-movie/add-movie.component.ts
+++ app/src/app/movies/add-movie/add-movie.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
-import { SelectButtonModule } from "primeng/selectbutton";
+import { SelectButtonModule } from 'primeng/selectbutton';
 import { DropdownModule } from 'primeng/dropdown';
 import { FileSelectEvent, FileUploadModule } from 'primeng/fileupload';
 import { MessageService } from 'primeng/api';
@@ -14,85 +14,123 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-add-movie',
   standalone: true,
-  imports: [FormsModule, InputTextModule, SelectButtonModule, DropdownModule, FileUploadModule, ProgressSpinnerModule],
+  imports: [
+    FormsModule,
+    InputTextModule,
+    SelectButtonModule,
+    DropdownModule,
+    FileUploadModule,
+    ProgressSpinnerModule,
+  ],
   templateUrl: './add-movie.component.html',
-  styleUrl: './add-movie.component.css'
+  styleUrl: './add-movie.component.css',
 })
 export class AddMovieComponent {
-  constructor(private router: Router, private messageService: MessageService, private movieService: MovieService) {
-
-  }
+  constructor(
+    private router: Router,
+    private messageService: MessageService,
+    private movieService: MovieService,
+  ) {}
   thumbnailSelected($event: FileSelectEvent) {
-    this.toBase64($event.files[0]).then((result) => this.thumbnail = result)
+    this.toBase64($event.files[0]).then((result) => (this.thumbnail = result));
   }
   movieSelected($event: FileSelectEvent) {
-    this.fileSize = $event.files[0].size
-    this.movie = $event.files[0]
-    console.log($event.files[0])
+    this.fileSize = $event.files[0].size;
+    this.movie = $event.files[0];
+    console.log($event.files[0]);
   }
 
-  options: any[] = [{ label: 'Movie', value: 'movie' }, { label: 'Series', value: 'series' }];
+  options: any[] = [
+    { label: 'Movie', value: 'movie' },
+    { label: 'Series', value: 'series' },
+  ];
   selectedOption: string = 'movie';
 
   uploading: boolean = false;
-  movieName: string = "";
-  year: string = "";
-  director: string = " ";
-  genre: { name: string, code: string } | null = null;
+  movieName: string = '';
+  year: string = '';
+  directors: string = '';
+  genre: { name: string; code: string } | null = null;
   genres = [
     { name: 'Action', code: 'action' },
     { name: 'Adventure', code: 'adventure' },
-    { name: 'History', code: 'history' }, { name: 'Comedy', code: 'comedy' },
+    { name: 'History', code: 'history' },
+    { name: 'Comedy', code: 'comedy' },
     { name: 'Romance', code: 'romance' },
-    { name: 'Western', code: 'western' }
+    { name: 'Western', code: 'western' },
   ];
-  description: string = "";
-  actors: string = "";
-  rating: string = "";
-  episode_number: string = "";
-  duration: string = "";
-  thumbnail: any
-  movie: File | null = null
-  fileSize: number = 0
+  description: string = '';
+  actors: string = '';
+  rating: string = '';
+  episode_number: string = '';
+  duration: string = '';
+  thumbnail: any;
+  movie: File | null = null;
+  fileSize: number = 0;
 
   done() {
-    if (this.movieName != "" && !isNaN(Number(this.year)) && this.director != "" && this.genre != null && this.description != "" && this.actors != "" && !isNaN(Number(this.rating)) && !isNaN(Number(this.duration)) && this.movie != null) {
-      if ((this.selectedOption == "series" && isNaN(Number(this.episode_number))) || Number(this.year) < 1800 || Number(this.year) > 2030 || Number(this.rating) <= 0.0 || Number(this.rating) > 10.0) {
+    if (
+      this.movieName != '' &&
+      !isNaN(Number(this.year)) &&
+      this.directors != '' &&
+      this.genre != null &&
+      this.description != '' &&
+      this.actors != '' &&
+      !isNaN(Number(this.rating)) &&
+      !isNaN(Number(this.duration)) &&
+      this.movie != null
+    ) {
+      if (
+        (this.selectedOption == 'series' &&
+          isNaN(Number(this.episode_number))) ||
+        Number(this.year) < 1800 ||
+        Number(this.year) > 2030 ||
+        Number(this.rating) <= 0.0 ||
+        Number(this.rating) > 10.0
+      ) {
         this.messageService.add({
           severity: 'error',
           summary: 'Invalid data',
           key: 'bc',
           detail: 'Please enter valid data!',
-          life: 2000
-        })
+          life: 2000,
+        });
         return;
       }
-      let ep = -1
-      if (this.selectedOption == "series") {
-        ep = Number(this.episode_number)
+      let ep = -1;
+      if (this.selectedOption == 'series') {
+        ep = Number(this.episode_number);
       }
 
       let movie: Movie = {
-        upload_status: "transcoding",
+        upload_status: 'transcoding',
         name: this.movieName,
         description: this.description,
         episode_number: ep,
         year: Number(this.year),
         genre: this.genre['code'],
-        director: this.director,
+        directors: this.directors.split(','),
         duration: Number(this.duration),
         rating: Number(this.rating),
         fileSize: this.fileSize,
-        actors: this.actors.split(","),
+        actors: this.actors.split(','),
         thumbnail: this.thumbnail,
         created_at: new Date().valueOf(),
         modified_at: this.movie?.lastModified || 0,
-        search_field: this.movieName + "%" + this.description + "%" + this.actors + "%" + this.director + "%" + this.genre['code']
-
-      }
+        search_field:
+          this.movieName +
+          '%' +
+          this.description +
+          '%' +
+          this.actors +
+          '%' +
+          this.directors +
+          '%' +
+          this.genre['code'],
+      };
       this.movieService.getUploadUrl(movie).subscribe({
         next: (data: UploadUrl) => {
-          console.log(data)
+          console.log(data);
           if (this.movie != null) {
             this.uploading = true;
 
@@ -104,24 +142,25 @@ export class AddMovieComponent {
                   summary: 'Upload finished',
                   key: 'bc',
                   detail: 'Movie uploaded successfully!',
-                  life: 2000
-                })
+                  life: 2000,
+                });
                 this.uploading = false;
-                this.router.navigate(['/catalog'])
+                this.router.navigate(['/catalog']);
               },
 
-              error: (err) => { console.log(err); this.showError() }
+              error: (err) => {
+                console.log(err);
+                this.showError();
+              },
             });
           }
         },
-        error: (err) => { console.log(err); this.showError() }
-      })
-
-
+        error: (err) => {
+          console.log(err);
+          this.showError();
+        },
+      });
     }
-
-
-
   }
 
   showError() {
@@ -130,8 +169,8 @@ export class AddMovieComponent {
       summary: 'Upload failed!',
       key: 'bc',
       detail: 'Server error!',
-      life: 2000
-    })
+      life: 2000,
+    });
   }
   toBase64 = (file: File) =>
     new Promise((resolve, reject) => {
--- cdk/resources/lambda/movie/upload.ts
+++ cdk/resources/lambda/movie/upload.ts
@@ -1,23 +1,33 @@
-import { APIGatewayProxyEvent, Context, APIGatewayProxyResult } from "aws-lambda";
-import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import {
+  APIGatewayProxyEvent,
+  Context,
+  APIGatewayProxyResult,
+} from "aws-lambda";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { Movie } from "../../../types";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { randomUUID } from "crypto";
 import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
 import { DynamoDB } from "@aws-sdk/client-dynamodb";
-const BUCKET_NAME = process.env.BUCKET_NAME || '';
-const TABLE_NAME = process.env.TABLE_NAME || '';
+const BUCKET_NAME = process.env.BUCKET_NAME || "";
+const TABLE_NAME = process.env.TABLE_NAME || "";
+const CREW_TABLE_NAME = process.env.CREW_TABLE_NAME || "";
 
 async function handler(event: APIGatewayProxyEvent, context: Context) {
-
   if (!event.body) {
-    return { statusCode: 400, body: 'invalid request, you are missing the parameter body' };
+    return {
+      statusCode: 400,
+      body: "invalid request, you are missing the parameter body",
+    };
   }
-  const item = typeof event.body == 'object' ? event.body : JSON.parse(event.body) as Movie;
+  const item =
+    typeof event.body == "object"
+      ? event.body
+      : (JSON.parse(event.body) as Movie);
   const db = DynamoDBDocument.from(new DynamoDB());
-  console.log(event)
+  console.log(event);
   try {
-    let REGION = 'eu-central-1';
+    let REGION = "eu-central-1";
     let bucket = BUCKET_NAME;
     let key = "";
     if (!item.id) {
@@ -30,7 +40,9 @@ async function handler(event: APIGatewayProxyEvent, context: Context) {
     const client = new S3Client({ region: REGION });
     const command = new PutObjectCommand({ Bucket: bucket, Key: bucketKey });
 
-    const presignedUrl = await getSignedUrl(client, command, { expiresIn: 500 });
+    const presignedUrl = await getSignedUrl(client, command, {
+      expiresIn: 500,
+    });
     console.log("PresignedUrl: ", presignedUrl);
 
     item.id = key;
@@ -41,9 +53,8 @@ async function handler(event: APIGatewayProxyEvent, context: Context) {
 
     await db.put(params);
 
-
-
-
+    // add to crew table
+    await putInCastAndCrewTable(item);
 
     const response: APIGatewayProxyResult = {
       statusCode: 200,
@@ -54,12 +65,25 @@ async function handler(event: APIGatewayProxyEvent, context: Context) {
     };
 
     return response;
-
   } catch (err) {
     console.error(err);
-    return { statusCode: 500, body: err }
+    return { statusCode: 500, body: err };
+  }
+}
+async function putInCastAndCrewTable(movie: Movie) {
+  const db = DynamoDBDocument.from(new DynamoDB());
+  for (const actor of movie.actors) {
+    await db.put({
+      TableName: CREW_TABLE_NAME,
+      Item: { id: actor + "A", movie_id: movie.id },
+    });
+    for (const director of movie.directors) {
+      await db.put({
+        TableName: CREW_TABLE_NAME,
+        Item: { id: director + "D", movie_id: movie.id },
+      });
+    }
   }
 }
 
 export { handler };
-
